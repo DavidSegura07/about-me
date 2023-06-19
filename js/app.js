961f6d2ce90827e4ab3694946bf2b47a67e169a7
David Segura <dsegura07@yahoo.com>
2023-06-19 14:54:31 -0700

    Got question 7 to work as well as the total points to work

--- js/app.js
+++ js/app.js
@@ -1,147 +1,163 @@
 'use strict';
 
-// // proof life
-// console.log('Hello');
+// proof life
+console.log('Hello');
 
 let userPoints = 0;
-// alert('Welcome');
 
-// let userName = prompt('What is your name?');
-// console.log(userName);
-// alert('Hello ' + userName + ' Lets play a guessing game.');
+alert('Welcome');
 
-// // Question 1
+let userName = prompt('What is your name?');
+console.log(userName);
+alert('Hello ' + userName + ' Lets play a guessing game.');
 
-// function checkIfILikeCars() {
-//   let doILikeCars = prompt('Do I like cars? Yes or No?').toLowerCase();
-//   if (doILikeCars === 'yes' || doILikeCars === 'y'){
-//     //console.log('Yes, I like cars');
-//     alert('Yes i like cars.');
-//   } else if (doILikeCars === 'no' || doILikeCars === 'n'){
-//     //console.log('That\'s incorrect');
-//     alert('That\'s incorrect.');
-//   } else {
-//     //console.log('You need to answer with a yes or no. Do I like cars?');
-//     alert('You need to answer with a yes or no. Do I like cars?');
-//   }
-// }
-// checkIfILikeCars();
+// Question 1
 
-// // Question 2
+function checkIfILikeCars() {
+  let doILikeCars = prompt('Do I like cars? Yes or No?').toLowerCase();
+  if (doILikeCars === 'yes' || doILikeCars === 'y'){
+    //console.log('Yes, I like cars');
+    alert('Yes i like cars.');
+    userPoints++;
+  } else if (doILikeCars === 'no' || doILikeCars === 'n'){
+    //console.log('That\'s incorrect');
+    alert('That\'s incorrect.');
+  } else {
+    //console.log('You need to answer with a yes or no. Do I like cars?');
+    alert('You need to answer with a yes or no. Do I like cars?');
+  }
+}
+checkIfILikeCars();
 
-// function checkBornCity() {
-//   let cityBorn = prompt('Was I born in Santa Ana? Yes or No?').toLowerCase();
-//   if (cityBorn === 'yes' || cityBorn === 'y'){
-//     //console.log('Yes, I was born in Santa Ana.');
-//     alert('Yes, I was born in Santa Ana');
-//   } else if (cityBorn === 'no' || cityBorn === 'n'){
-//     //console.log('That\' incorrect);
-//     alert('That\' incorrect.');
-//   } else {
-//     //console.log('You need to answer with a yes or no. Was I born in Santa Ana? Yes or No?');
-//     alert('You need to answer with a yes or no. Was I born in Santa Ana? Yes or No?');
-//   }
-// }
-// checkBornCity();
+// Question 2
 
-// // Question 3
+function checkBornCity() {
+  let cityBorn = prompt('Was I born in Santa Ana? Yes or No?').toLowerCase();
+  if (cityBorn === 'yes' || cityBorn === 'y'){
+    //console.log('Yes, I was born in Santa Ana.');
+    alert('Yes, I was born in Santa Ana');
+    userPoints++;
+  } else if (cityBorn === 'no' || cityBorn === 'n'){
+    //console.log('That\' incorrect);
+    alert('That\' incorrect.');
+  } else {
+    //console.log('You need to answer with a yes or no. Was I born in Santa Ana? Yes or No?');
+    alert('You need to answer with a yes or no. Was I born in Santa Ana? Yes or No?');
+  }
+}
+checkBornCity();
 
-// function myFavCars() {
-//   let favoriteCar = prompt('Is the BMW M3 my favorite car? Yes or No?').toLowerCase();
-//   if (favoriteCar === 'yes' || favoriteCar === 'y'){
-//     //console.log('Yes that\'s correct');
-//     alert('Yes that\'s correct');
-//   } else if (favoriteCar === 'no' || favoriteCar === 'n'){
-//     //console.log('That\'s incorrect');
-//     alert('That\'s incorrect');
-//   } else {
-//     //console.log('You need to answer with a yes or no. Is the BMW M3 my favorite car? Yes or No?);
-//     alert('You need to answer with a yes or no. Is the BMW M3 my favorite car? Yes or No?');
-//   }
-// }
-// myFavCars();
+// Question 3
 
-// // Question 4
+function myFavCars() {
+  let favoriteCar = prompt('Is the BMW M3 my favorite car? Yes or No?').toLowerCase();
+  if (favoriteCar === 'yes' || favoriteCar === 'y'){
+    //console.log('Yes that\'s correct');
+    alert('Yes that\'s correct');
+    userPoints++;
+  } else if (favoriteCar === 'no' || favoriteCar === 'n'){
+    //console.log('That\'s incorrect');
+    alert('That\'s incorrect');
+  } else {
+    //console.log('You need to answer with a yes or no. Is the BMW M3 my favorite car? Yes or No?);
+    alert('You need to answer with a yes or no. Is the BMW M3 my favorite car? Yes or No?');
+  }
+}
+myFavCars();
 
-// function hatePhoto() {
-//   let hatePhotography = prompt('Do i hate photography? Yes or No').toLowerCase();
-//   if (hatePhotography === 'yes' || hatePhotography ==='y'){
-//     //console.log('No that\'s incorrect');
-//     alert('No that\'s incorrect');
-//   } else if (hatePhotography === 'no' || hatePhotography === 'n'){
-//     //console.log('No, I love photography');
-//     alert('No, I love photography');
-//   } else {
-//     //console.log('You need to answer with a yes or no. Do i hate photography? Yes or No?);
-//     alert('You need to answer with a yes or no. Do i hate photography? Yes or No?');
-//   }
-// }
-// hatePhoto();
+// Question 4
 
-// // Question 5
+function hatePhoto() {
+  let hatePhotography = prompt('Do i hate photography? Yes or No').toLowerCase();
+  if (hatePhotography === 'yes' || hatePhotography ==='y'){
+    //console.log('No that\'s incorrect');
+    alert('No that\'s incorrect');
+  } else if (hatePhotography === 'no' || hatePhotography === 'n'){
+    //console.log('No, I love photography');
+    alert('No, I love photography');
+    userPoints++;
+  } else {
+    //console.log('You need to answer with a yes or no. Do i hate photography? Yes or No?);
+    alert('You need to answer with a yes or no. Do i hate photography? Yes or No?');
+  }
+}
+hatePhoto();
 
-// function colorBlack() {
-//   let secondFavoriteColor = prompt('Is black my favorite color? Yes or No?').toLowerCase();
-//   if (secondFavoriteColor === 'yes' || secondFavoriteColor === 'y'){
-//     //console.log('No, that\'s incorrect ');
-//     alert('No, that\'s incorrect ');
-//   } else if (secondFavoriteColor === 'No' || secondFavoriteColor === 'n' ){
-//     //console.log('That\'s Correct');
-//     alert('That\'s Correct');
-//   } else {
-//     //console.log('You need to answer with a yes or no.Is black my favorite color? Yes or No?');
-//     alert('You need to answer with a yes or no. Is black my favorite color? Yes or No?');
-//   }
-// }
-// colorBlack();
+// Question 5
 
-// // Question 6
+function colorBlack() {
+  let secondFavoriteColor = prompt('Is black my favorite color? Yes or No?').toLowerCase();
+  if (secondFavoriteColor === 'yes' || secondFavoriteColor === 'y'){
+    //console.log('No, that\'s incorrect ');
+    alert('No, that\'s incorrect ');
+  } else if (secondFavoriteColor === 'No' || secondFavoriteColor === 'n' ){
+    //console.log('That\'s Correct');
+    alert('That\'s Correct');
+    userPoints++;
+  } else {
+    //console.log('You need to answer with a yes or no.Is black my favorite color? Yes or No?');
+    alert('You need to answer with a yes or no. Is black my favorite color? Yes or No?');
+  }
+}
+colorBlack();
 
-// function pickCorrectNumber() {
-//   let attempts = 4;
-//   let correctNumber = 7;
+// Question 6
 
-//   for (let i = 0; i < attempts; i++) {
-//     let userGuess = prompt(`Pick a number from 1 to 10. (You have ${attempts - i} attemps reamining):`);
-//     userGuess = parseInt(userGuess);
-//     console.log(correctNumber);
-//     console.log(userGuess);
-//     console.log(typeof userGuess);
-//     if (userGuess < correctNumber) {
-//       alert('Too Low!');
-//     } else if (userGuess > correctNumber) {
-//       alert('Too High');
-//     } else if (userGuess === correctNumber) {
-//       alert('Just right');
-//       break;
-//     }
-//   }
-//   alert('The answer is 7');
-// }
-// pickCorrectNumber();
+function pickCorrectNumber() {
+  let attempts = 4;
+  let correctNumber = 7;
 
-// // Question 7
-
-
-let favCarsAnswers = ['BMW E36', 'BMW E30', 'Porsche 997', 'BMW E46', 'Toyota AE86 Trueno', 'Nissan 240 S14', 'Nissan 240 S13', 'Mazda RX7 FD', 'Nissan R32 Skyline', 'Ford Mustang 1968'];
-let attemptsToGo = 6;
-
-for (let i = 0; i < attemptsToGo; i++) {
-  console.log(attemptsToGo);
-  let userGuess = prompt(`what is one of my favorite cars? You have  ${attemptsToGo - i - 1}   left.`);
-  console.log(attemptsToGo);
-  for (let j = 0; j < favCarsAnswers.length; j++) {
-    console.log(favCarsAnswers[j].toLowerCase);
-    console.log(typeof favCarsAnswers);
-    if (userGuess === favCarsAnswers[j].toLowerCase()) {
-      console.log('You are correct!');
-      alert('You are correct!');
+  for (let i = 0; i < attempts; i++) {
+    let userGuess = prompt(`Pick a number from 1 to 10. (You have ${attempts - i} attemps reamining):`);
+    userGuess = parseInt(userGuess);
+    console.log(correctNumber);
+    console.log(userGuess);
+    console.log(typeof userGuess);
+    if (userGuess < correctNumber) {
+      alert('Too Low!');
+    } else if (userGuess > correctNumber) {
+      alert('Too High');
+    } else if (userGuess === correctNumber) {
+      alert('Just right');
+      userPoints++;
       break;
     }
-    else {
+  }
+  alert('The answer is 7');
+}
+pickCorrectNumber();
+
+// Question 7
+
+function userGuessCorrectCars() {
+  let favCarsAnswers = ['BMW E36', 'BMW E30', 'Porsche 997', 'BMW E46', 'Toyota AE86 Trueno', 'Nissan 240 S14', 'Nissan 240 S13', 'Mazda RX7 FD', 'Nissan R32 Skyline', 'Ford Mustang 1968'];
+  let attemptsToGo = 6;
+  let userGuessCorrect = false;
+  for (let i = 0; i < attemptsToGo; i++) {
+    console.log(attemptsToGo);
+    let userGuess = prompt(`what is one of my favorite cars? You have  ${attemptsToGo - i - 1}   left.`);
+    console.log(attemptsToGo);
+    for (let j = 0; j < favCarsAnswers.length; j++) {
+      console.log(favCarsAnswers[j].toLowerCase);
+      console.log(typeof favCarsAnswers);
+      if (userGuess === favCarsAnswers[j].toLowerCase()) {
+        console.log('You are correct!');
+        alert('You are correct!');
+        userPoints++;
+        userGuessCorrect = true;
+        attemptsToGo = 0;
+        break;
+      }
+    }
+    if (userGuessCorrect === false && attemptsToGo > 0) {
       console.log(`No try again. You have  ${attemptsToGo - i - 1}   left.`);
+      alert(`No try again. You have  ${attemptsToGo - i - 1}   left.`);
     }
   }
 }
 
+userGuessCorrectCars();
 
+let totalPoint = userPoints;
+console.log(totalPoint);
+alert (`you got ${totalPoint} correct out of 7 questions asked`);
